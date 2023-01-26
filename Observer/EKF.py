d9e18d7448003857e21c19d36d4a4339d60bf43d
Harald <hamo@stud.ntnu.no>
2023-01-26 13:00:47 +0100

    Further developing

--- Observer/EKF.py
+++ Observer/EKF.py
@@ -7,23 +7,38 @@ from utils import J, Rz, Smat, pipi
 class EKF(Observer):
     '''
     EKF
+
+    x_hat = [
+        zeta    (6DOF)
+        eta     (3DOF)
+        nu      (3DOF)
+        bias    (3DOF)
+    ]
+
+
     '''
-    def __init__(self, x0=np.zeros(1), P0 = np.zeros((1,1)), dt=0.1):
+    def __init__(self, dt, Minv, D, x0=np.zeros(1), P0 = np.zeros((1,1))):
+        # Tuning
         self._Qd = 0
         self._Rd = 0
 
-        self._phi = 0
-        self._gamma = 0
-        self._H = 0
+        # Constant matrices
+        self._Minv = Minv
+        self._D = D
+        self._H, self._B, self._E = self.initialize_constant_matrices()
+        
 
         self._dt = dt
 
-        self._xhat = np.zeros(1)
+        self._DOF = 3
+
+        self._xhat = np.zeros(15)
         self._xbar = x0
 
         self._Pbar = P0
         self._Phat = np.zeros((1,1))
 
+        
 
 
     def predictor(self, tau):
@@ -56,13 +71,40 @@ class EKF(Observer):
         return K
 
 
-    def transition_function(self, state, tau, noise):              # f(x,u,w)
-        pass
-    def transition_function_jacobian(self, state, tau, noise):     # df/dx
-        pass
-    def measurement_function(self):             # h(x,u)
-        pass
-    def measurement_function_jacobian(self):    # dh/dx
-        pass
+    def state_function(self, x, tau, noise):                    # f(x,u,w)
+        # f = Ax + Bu + Ew
+        raise NotImplementedError
+
+    def state_function_jacobian(self, state, tau, noise):       # df/dx
+         raise NotImplementedError
+
+    def state_function_noise_jacobian(self):
+        raise NotImplementedError                               # df/dw
+
+    def measurement_function(self, x):                          # h(x,u)
+        h = self._H @ x
+        raise h
+
+    def measurement_function_jacobian(self):                    # dh/dx
+        raise NotImplementedError
 
 
+    def initialize_constant_matrices():
+        H = np.zeros((3,15))
+        B = np.zeros((15,3))
+        E = np.zeros((15,6))
+        # Aw = 
+        # Ew = 
+        # Eb = 
+        return H, B, E
+
+
+'''
+Yet to do:
+
+    - Implement constant matrices
+    - Implement state function
+    - Implement jacobians
+    - Discretize (phi and gamma)
+
+'''
