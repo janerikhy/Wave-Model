2e3d2e25f9a14c5027b90da8853d921c40c0ef14
mariefko <marie.kongshaug@gmail.com>
2023-02-01 13:33:01 +0100

    add thruster-class

---
+++ src/MCSimPython/thrust_allocation/thruster.py
@@ -0,0 +1,41 @@
+import numpy as np
+from constraints import Constraint
+ 
+class Thruster:
+
+    def __init__(self, pos, K):
+        self._r = np.array(pos)
+        self._K = K
+        self._u = np.array([0, 0])
+
+    @property
+    def pos_x(self):
+        """
+        x-position of thruster
+        """
+        return self._r[0]
+
+    @property
+    def pos_y(self):
+        """
+        y-position of thruster
+        """
+        return self._r[1]
+
+
+class TunnelThruster(Thruster):
+
+    def __init__(self, pos, max_thrust, angle):
+        super().__init__(pos)
+
+        self._max_thrust = max_thrust
+        self._angle = angle
+
+class AzimuthThruster(Thruster):
+
+    def __init__(self, pos, max_thrust, rotation):
+        super().__init__(pos)
+
+        self._max_thrust = max_thrust
+        self._rotation = rotation
+
