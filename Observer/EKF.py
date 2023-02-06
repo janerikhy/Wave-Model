c33a2c120013a2fa700e63f4e3893c978f6437b5
Harald <hamo@stud.ntnu.no>
2023-02-06 17:03:40 +0100

    Tuning work done

--- Observer/EKF.py
+++ Observer/EKF.py
@@ -6,7 +6,7 @@ from utils import J, Rz, Smat, pipi
 
 class EKF(Observer):
     '''
-    Implementation of Extended Kalman Filter (EKF).
+    Implementation of Extended Kalman Filter (EKF) for CSAD.
 
     x_hat = [
         zeta    (6DOF)
@@ -21,53 +21,55 @@ class EKF(Observer):
 
         Input:
             - dt: Time step
-            - M: Inertia matrix of system (including added mass)
-            - D: Full damping matrix of system
+            - M: Inertia matrix of system (including added mass) 6DOF
+            - D: Full damping matrix of system 6DOF
         '''
         self._dt = dt
 
-        # Tuning
-        self._Qd = 10 * np.array([
-            [1,0,0,0,0,0],
-            [0,1,0,0,0,0],
-            [0,0,0.2*np.pi/180,0,0,0],
-            [0,0,0,1e2,0,0],
-            [0,0,0,0,1e2,0],
+        # Tuning - Done manually (as of now)
+        self._Qd = np.array([
+            [1e-3,0,0,0,0,0],
+            [0,1e-3,0,0,0,0],
+            [0,0,.2*np.pi/180,0,0,0],
+            [0,0,0,5e3,0,0],
+            [0,0,0,0,5e3,0],
             [0,0,0,0,0,1e3]
         ])
-        self._Qd = np.eye(6) *.1
-        self._Rd = 0.5**2* np.array([
+        self._Rd = np.array([
             [1,0,0],
             [0,1,0],
-            [0,0,1]
+            [0,0,10*np.pi/180]
         ])
-        self._Rd = np.eye(3)
+
+        #self._Qd = np.eye(6)*.01
+        #self._Rd = np.eye(3)
+
         # Constant matrices
-        i = np.ix_([0,1,5],[0,1,5])     # Extract surge-sway-yaw DOFs
+        i = np.ix_([0,1,5],[0,1,5])                                         # Extract surge-sway-yaw DOFs
+
         M = M[i]
-        self._Minv = np.linalg.inv(M)
-        D = D[i]
-        self._D = D
+        self._Minv = np.linalg.inv(M)                                       # 3DOF inverted mass matrix
+        self._D = D[i]                                                      # 3DOF damping matrix
+        
         self._H, self._B, self._E, self._Aw, self._gamma = 0,0,0,0,0
         self.initialize_constant_matrices()
         
-        # self._DOF = 3
-
+        
+        # Initial values
         self._xhat = np.zeros(15)
         self._xbar = x0
 
-        self._Pbar = P0
-        self._Phat = np.eye(15)
+        self._Pbar = np.zeros((15,15))      #P0
+        self._Phat = np.zeros((15,15))
 
     def predictor(self, tau):
         '''
         Documentation
         '''
         phi = self.state_function_jacobian()
-        # gamma = self.state_function_noise_jacobian()
         f = self.state_function(self._xhat, tau, np.zeros(6))
 
-        self._Pbar = phi@self._Phat@phi.T + self._gamma@self._Qd@self._gamma.T
+        self._Pbar = phi@self._Phat@(phi.T) + self._gamma@self._Qd@(self._gamma.T)
         self._xbar = self._xhat + self._dt * f
 
 
@@ -75,29 +77,29 @@ class EKF(Observer):
         '''
         Documentation
         '''
-        #H = self.measurement_function_jacobian()
         K = self.EKF_gain
 
         parenthesis = np.eye(15) - K@self._H
+        measurement_error = y - (self._H@self._xbar)
+        measurement_error[2] = pipi(measurement_error[2])
 
-        self._Phat = parenthesis@self._Pbar@parenthesis.T + K@self._Rd@K.T
-        self._xhat = self._xbar + K @(y - self._H@self._xbar)
+        self._Phat = parenthesis@self._Pbar@(parenthesis.T) + K@self._Rd@(K.T)
+        self._xhat = self._xbar + K@measurement_error
         
-
 
     @property
     def EKF_gain(self):
         '''
-        Documentation
+        Calculate Kalman gain K, weighting the measurements and the internal model
         '''
-        a = self._H@self._Pbar@(self._H.T) + self._Rd
-        K = self._Pbar@(self._H.T)@np.linalg.inv(a)
+        parenthesis = self._H@self._Pbar@(self._H.T) + self._Rd
+        K = self._Pbar@(self._H.T)@np.linalg.inv(parenthesis)
         return K
 
 
     def state_function(self, x, tau, noise):                    # f(x,u,w)
         '''
-        Documentation
+        x_dot = f(x,u,w) = A(x) + B*tau + E*w
         '''
         xi = x[0:6]
         eta = x[6:9]
@@ -105,34 +107,33 @@ class EKF(Observer):
         b = x[9:12]
         nu = x[12:15]
 
-        Ax1 = np.array(
+        Ax = np.array(
             [
             (self._Aw@xi),
             (Rz(psi)@nu),
             np.zeros(3),
             (-self._Minv@self._D@nu + self._Minv@(Rz(psi).T)@b)     # plus or minus in 2nd term?
-            ]
+            ], dtype=object
         )
-        Ax = np.concatenate(Ax1)
+        Ax = np.concatenate(Ax)                                     # Convert to 15x1
+
         f = Ax + self._B@tau + self._E@noise
         return f
 
 
     def state_function_jacobian(self):       # df/dx
         '''
-        Documentation
         phi = eye(15) + dt * del(f)/del(x)
 
         where
 
         del(f)/del(x) = [
-            Aw          0_(6x3)     0_(6x3)         0_(6x3)                 row (1)
-            0_(3x6)     del_f2      0_(3x3)         R(psi)                  row (2)
-            0_(3x6)     0_(3x3)     0_(3x3)         0_(3x3)                 row (3)
-            0_(3x6)     del_f4      M_inv*R(psi).T  -M_inv*D                row (4)
+            row (1):        A_w         0_(6x3)     0_(6x3)         0_(6x3)                 
+            row (2):        0_(3x6)     del_f2      0_(3x3)         R(psi)                  
+            row (3):        0_(3x6)     0_(3x3)     0_(3x3)         0_(3x3)                
+            row (4):        0_(3x6)     del_f4      M_inv*R(psi).T  -M_inv*D                
         ]   
         '''
-        phi = np.zeros((15,15))
 
         psi = self._xhat[8]
         b1 = self._xhat[9]
@@ -189,29 +190,54 @@ class EKF(Observer):
 
     def initialize_constant_matrices(self):
         '''
-        x_dot = f(x,u,w) 
+        Initialize following matrices:
+
+        A_w = [
+            0_(3x3)     I_3
+            -omega^2    -2*zeta*omega
+        ]
+
+        E = [
+            0_(3x3)     0_(3x3)
+            E_w         0_(3x3)
+            0_(3x3)     E_b
+            0_(3x3)     0_(3x3)
+        ]
+
+        gamma = dt * E
+
+        H = [
+            0_(3x3)     I_3     I_3     0_(3x6)
+        ]
+
+        B = [
+            0_(12x3)
+            M_inv
+        ]
         '''
-        # Aw
-        Tp = 1e7    # Wave period
+        # A_w
+        Tp = 1e7                                                # Wave period
         omega = 2*np.pi/Tp
-        zeta = 0.05
+        zeta = 0.05                                             # Damping coeff
+        # Define A_w = [
+        # Aw1   Aw2
+        # Aw3   Aw4
+        # ]
         Aw1 = np.zeros((3,3))
         Aw2 = np.eye(3)
         Aw3 = -omega**2*np.eye(3)
         Aw4 = -2*zeta*omega*np.eye(3)
-        #self._Aw = np.bmat([[Aw1, Aw2],[Aw3, Aw4]])
-        self._Aw = np.concatenate((np.concatenate((Aw1, Aw2), axis=1),np.concatenate((Aw3, Aw4), axis=1)), axis=0)  # Should probably be made more readable
-
+        self._Aw = np.concatenate((np.concatenate((Aw1, Aw2), axis=1),np.concatenate((Aw3, Aw4), axis=1)), axis=0)  # Should probably be made more readable. Use np.block
+        
         # E
         self._E = np.zeros((15,6))
-        Ew = np.diag([1,1,1])*.01
+        Ew = np.diag([1,1,1])
         Eb = np.eye(3)
         self._E[3:6,0:3] = Ew 
         self._E[9:12,3:6] = Eb
 
         # Gamma
         self._gamma = self._dt * self._E
-
 
         # H
         self._H = np.zeros((3,15))
@@ -222,9 +248,13 @@ class EKF(Observer):
         self._B = np.zeros((15,3))
         self._B[12:15, 0:3] = self._Minv
         
-
-    def get_xhat(self):
-        return super().get_xhat()
+    @property
+    def x_hat(self):
+        return self._xhat
+    @property
+    def P_hat(self):
+        return self._Phat
+    
 
 '''
 "Tuning factors":
@@ -236,5 +266,4 @@ class EKF(Observer):
     - Adjustment of noise
     - Time step
     - Initial values
-
 '''
