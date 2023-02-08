06b672fe6607fd9e486b97e600f961b2c4a6bc2a
mariefko <marie.kongshaug@gmail.com>
2023-02-08 14:11:35 +0100

    add rate limit function

--- src/MCSimPython/simulator/thruster_dynamics.py
+++ src/MCSimPython/simulator/thruster_dynamics.py
@@ -7,21 +7,29 @@ thrust_max = 1.5            #[N]
 
 class ThrusterDynamics:
 
-    def __init__(self, u, K):
+    def __init__(self, u, K, dt):
         self._u = u
         self._K = K
-
+        self._dt = dt
 
     def saturate(self, signal, min, max):
         return np.clip(signal, min, max)
+
+    def limit_rate(self, signal_curr, signal_prev, max):
+        if (np.abs(signal_curr-signal_prev)/self._dt > max):
+            signal_curr = signal_prev + abs(max * self._dt)
+        return signal_curr
 
     def propeller_rev(self):
         n = np.sign(self._u) * np.sqrt(np.abs(self._u))
         return n
     
-    def compute_tau(self):
-        n = self.propeller_rev()
+    def compute_tau(self, n):
         tau = self._K @ np.abs(n) @ n
         return tau
+    
+    def get_tau(self):
+        n = self.propeller_rev()
+
 
 
