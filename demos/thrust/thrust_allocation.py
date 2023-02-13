3ff5382b49c001e9e1a28cc6b90e18bec9f804be
mariefko <marie.kongshaug@gmail.com>
2023-02-13 10:13:26 +0100

    allocate without constraints

--- demos/thrust/thrust_allocation.py
+++ demos/thrust/thrust_allocation.py
@@ -2,6 +2,7 @@ import MCSimPython.thrust_allocation.allocation as al
 import MCSimPython.simulator.thruster_dynamics as dynamics
 import MCSimPython.thrust_allocation.thruster as thruster
 import MCSimPython.vessel_data.CSAD.thruster_data as data 
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -19,20 +20,28 @@ for i in range(6):
 print(allocator.n_thrusters)
 print(allocator.n_problem)
 
-# Set random desired force (tau_d)
+# Set random desired force (tau_d) - here tested with two "time steps"
 
-tau_d = [10, 20, 5]
+tau_d = np.array([[10, 15, 5], [4, 6, 9], [8, 4, 7]])
 
-u, alpha = allocator.allocate(tau_d)
+Fx = []
+Fy = []
+Mz = []
 
-print(u)
 
-ThrustDyn = dynamics.ThrusterDynamics(u, alpha, dt=0.2)
+for i in range(len(tau_d)):
+    u, alpha = allocator.allocate(tau_d[i])
+    ThrustDyn = dynamics.ThrusterDynamics(u, alpha, dt=0.2)
+    Fx.append(ThrustDyn.get_tau(u, alpha)[0])
+    #print(Fx[i])
+    Fy.append(ThrustDyn.get_tau(u, alpha)[1])
+    Mz.append(ThrustDyn.get_tau(u, alpha)[2])
 
-tau = ThrustDyn.get_tau(u, alpha)
-
-print(tau)
-
-#figure, axis = plt.subplots(2, 1)
-#axis[0, 0].plot(tau)
-#plt.show()
+fig, axs = plt.subplots(2)
+fig.suptitle('Thrust allocation')
+axs[0].plot(Fx, label='Fx')
+axs[0].plot(Fy, label='Fy')
+axs[0].plot(Mz, label='Fz')
+axs[1].plot(Fy)
+axs[0].legend()
+plt.show()
