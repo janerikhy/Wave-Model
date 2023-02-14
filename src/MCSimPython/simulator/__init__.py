cf63ac952e8b7bc2b2f06097d4f3e649e66d64c1
Jan-Erik Hygen <j-e.hygen@hotmail.com>
2023-02-14 19:02:33 +0100

    Add short description in __init__.py simulator

---
+++ src/MCSimPython/simulator/__init__.py
@@ -0,0 +1,17 @@
+"""
+Vessel Simulation Models (:mod:`MCSimPython.simulator`)
+================================================
+
+The MCSimPython simulator package contains a set of 
+different simulation models.
+
+Contents
+---------
+The following modules are found in the `MCSimPython.simulator`.
+
+- vessel.py        --- Base class for vessel simulation models.
+
+- csad.py          --- Simulation models for C/S Arctic Drillship.
+
+- gunnerus.py      --- Simulation models for R/V Gunnerus.
+"""
