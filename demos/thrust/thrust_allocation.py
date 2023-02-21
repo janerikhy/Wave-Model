7c701a891417571b4654e05295bfeba6be6ba036
mariefko <marie.kongshaug@gmail.com>
2023-02-21 14:00:48 +0100

    remove redundant content

--- demos/thrust/thrust_allocation.py
+++ demos/thrust/thrust_allocation.py
@@ -7,41 +7,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-allocator = al.AllocatorCSAD()
-
+allocator = al.fixed_angle_allocator()
 
 # Set thruster configuration
 
 for i in range(6):
     allocator.add_thruster(thruster.Thruster((data.lx[i], data.ly[i]), data.K[i]))
 
-# Check that all thrusters are added - in this case there should be 6 thrusters and 12 problems (thrusters * 2)
+# Check that all thrusters are added - in this case there should be 6 thrusters
 
 print(allocator.n_thrusters)
-print(allocator.n_problem)
 
-# Set random desired force (tau_d) - here tested with two "time steps"
+# Set random desired force (tau_d)
 
-tau_d = np.array([[10, 15, 5], [4, 6, 9], [8, 4, 7]])
+tau_d = np.array([[1, 0.3, 0.5], [0, 0.2, 0.7], [1, 3, 1]])
 
-Fx = []
-Fy = []
-Mz = []
+# First check that tau = tau_d when there are no constraints
 
 
 for i in range(len(tau_d)):
     u, alpha = allocator.allocate(tau_d[i])
     ThrustDyn = dynamics.ThrusterDynamics(u, alpha, dt=0.2)
-    Fx.append(ThrustDyn.get_tau(u, alpha)[0])
-    #print(Fx[i])
-    Fy.append(ThrustDyn.get_tau(u, alpha)[1])
-    Mz.append(ThrustDyn.get_tau(u, alpha)[2])
+    tau = ThrustDyn.get_tau(u, alpha)
 
-fig, axs = plt.subplots(2)
-fig.suptitle('Thrust allocation')
-axs[0].plot(Fx, label='Fx')
-axs[0].plot(Fy, label='Fy')
-axs[0].plot(Mz, label='Fz')
-axs[1].plot(Fy)
-axs[0].legend()
-plt.show()
+# Get tau when u set constraints for u
+
+for i in range(len(tau_d)):
+    u, alpha = allocator.allocate(tau_d[i])
+    ThrustDyn = dynamics.ThrusterDynamics(u, alpha, dt=0.2)
+    u = ThrustDyn.saturate(u, -1, 1) 
+    tau_sat = ThrustDyn.get_tau(u, alpha)
+    
