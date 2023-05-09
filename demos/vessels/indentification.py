84b3baf3bbdf777bbecd1f39acef39a30168cce0
Jan-Erik Hygen <j-e.hygen@hotmail.com>
2023-05-09 13:17:19 +0200

    Add user input for file name

--- demos/vessels/indentification.py
+++ demos/vessels/indentification.py
@@ -6,6 +6,16 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Get desired file name
+file_name = input("Enter file name: ")
+if not file_name.endswith('.json'):
+    file_name += '.json'
+vessel_name = input("Enter vessel name (gunnerus / CSAD): ")
+if vessel_name not in ['gunnerus', 'CSAD']:
+    raise ValueError("Vessel name must be either 'gunnerus' or 'CSAD'.")
+basedir = os.getcwd()
+rvg_dir = os.path.join(basedir, 'src', 'MCSimPython', 'vessel_data', vessel_name)
+
 vessel = RVG_DP_6DOF(0.1, config_file="rvg3_config.json")
 with open(vessel._config_file, 'r') as f:
     config = json.load(f)
@@ -20,9 +30,6 @@ with np.printoptions(precision=3, suppress=True):
     
     
 # Save the data to a configuration file to use it for simulation.
-file_name = "rvg3ABC_config.json"
-basedir = os.getcwd()
-rvg_dir = os.path.join(basedir, 'src', 'MCSimPython', 'vessel_data', 'gunnerus')
 
 vesselABC = {}
 vesselABC['Ar'] = sum(Ar, [])   # Flatten the list of lists (as in MATLAB)
